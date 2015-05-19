221bb265ceca384c0b8bff4891c6f3de3118d421
caryoscelus <caryoscelus@gmx.com>
2015-05-19 21:29:30 +0300

    controller

---
+++ dracykeiton/controller.py
@@ -0,0 +1,68 @@
+##
+##  Copyright (C) 2015 caryoscelus
+##
+##  This file is part of Dracykeiton
+##  https://github.com/caryoscelus/dracykeiton
+##  
+##  This program is free software: you can redistribute it and/or modify
+##  it under the terms of the GNU General Public License as published by
+##  the Free Software Foundation, either version 3 of the License, or
+##  (at your option) any later version.
+##  
+##  This program is distributed in the hope that it will be useful,
+##  but WITHOUT ANY WARRANTY; without even the implied warranty of
+##  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+##  GNU General Public License for more details.
+##  
+##  You should have received a copy of the GNU General Public License
+##  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+##
+
+"""Controller: 
+"""
+
+import functools
+
+from savable import Savable
+from entity import Entity
+
+class ControllableEntity(Entity):
+    def __init__(self):
+        super(ControllableEntity, self).__init__()
+        self.dynamic_property('available_actions', empty=set())
+
+class Controller(Savable):
+    def __init__(self):
+        super(Controller, self).__init__()
+        self._entities = set()
+    
+    def add_entity(self, entity):
+        self._entities.add(entity)
+    
+    def remove_entity(self, entity):
+        self._entities.remove(entity)
+    
+    def start_acting(self):
+        pass
+    
+    def act(self):
+        return None
+
+class Action(Savable):
+    def act(self):
+        pass
+
+class SimpleAction(Action):
+    def __init__(self, f):
+        super(SimpleAction, self).__init__()
+        self.f = f
+    
+    def act(self, *args, **kwargs):
+        self.f(*args, **kwargs)
+
+def simpleaction(f):
+    """Decorator"""
+    @functools.wraps(f)
+    def wrap(*args, **kwargs):
+        return SimpleAction(functools.partial(f, *args, **kwargs))
+    return wrap
---
+++ dracykeiton/test_controller.py
@@ -0,0 +1,48 @@
+##
+##  Copyright (C) 2015 caryoscelus
+##
+##  This file is part of Dracykeiton
+##  https://github.com/caryoscelus/dracykeiton
+##  
+##  This program is free software: you can redistribute it and/or modify
+##  it under the terms of the GNU General Public License as published by
+##  the Free Software Foundation, either version 3 of the License, or
+##  (at your option) any later version.
+##  
+##  This program is distributed in the hope that it will be useful,
+##  but WITHOUT ANY WARRANTY; without even the implied warranty of
+##  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+##  GNU General Public License for more details.
+##  
+##  You should have received a copy of the GNU General Public License
+##  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+##
+
+"""tests for controller.py"""
+
+from controller import Controller, ControllableEntity, simpleaction
+
+def test_controller():
+    controller = Controller()
+    entity = ControllableEntity()
+    controller.add_entity(entity)
+
+class CustomController(Controller):
+    def __init__(self):
+        super(CustomController, self).__init__()
+    
+    def act(self):
+        return ((entity, self.n1_action()) for entity in self._entities)
+    
+    @simpleaction
+    def n1_action(self, target):
+        target.n += 1
+
+def test_custom_controller():
+    controller = CustomController()
+    entity = ControllableEntity()
+    entity.dynamic_property('n', empty=0)
+    controller.add_entity(entity)
+    for (e, action) in controller.act():
+        action.act(e)
+    assert entity.n == 1
