fce90df75ec918b567e84e2231b4762abfb57b1a
caryoscelus <caryoscelus@gmx.com>
2015-06-08 21:45:05 +0300

    fix run-all-tests to actually return failure code

--- run-all-tests.sh
+++ run-all-tests.sh
@@ -4,10 +4,15 @@ pushd `dirname $0`
 
 source env/bin/activate
 py.test dracykeiton/
+PY3=$?
 deactivate
 
 source env2/bin/activate
 py.test dracykeiton/
+PY2=$?
 deactivate
 
 popd
+
+[ $PY2 -eq 0 ] && [ $PY3 -eq 0 ] && exit 0
+exit 1
