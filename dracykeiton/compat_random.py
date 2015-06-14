027e38dc3a5a376376ebe1b53b3956eda90c553f
caryoscelus <caryoscelus@gmx.com>
2015-06-14 23:17:18 +0300

    compat_random

---
+++ dracykeiton/compat_random.py
@@ -0,0 +1,33 @@
+##
+##  Copyright (C) 2015 caryoscelus
+##
+##  This file is part of Dracykeiton
+##  https://github.com/caryoscelus/dracykeiton
+##  
+##  This program is free software: you can redistribute it and/or modify
+##  it under the terms of the GNU General Public License as published by
+##  the Free Software Foundation, either version 3 of the License, or
+##  (at your option) any later version.
+##  
+##  This program is distributed in the hope that it will be useful,
+##  but WITHOUT ANY WARRANTY; without even the implied warranty of
+##  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+##  GNU General Public License for more details.
+##  
+##  You should have received a copy of the GNU General Public License
+##  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+##
+
+"""Exports appropriate random module.
+
+For now if Ren'Py is present, use its random, otherwise regular one.
+Usage: from compat_random import random.
+
+TODO: perhaps would make sense to just have module random reexporting content
+of appropriate module, but that's when there's proper packaging.
+"""
+
+try:
+    import renpy.random as random
+except ImportError:
+    import random
