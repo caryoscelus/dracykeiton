61b86f6232923128fa530412653b83618f79ec7d
caryoscelus <caryoscelus@gmx.com>
2015-06-21 13:41:45 +0300

    add Level/Xp entities

--- dracykeiton/common/xp.py
+++ dracykeiton/common/xp.py
@@ -18,12 +18,31 @@
 ##  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ##
 
-"""common: package containing common enitty build blocks"""
+"""Xp, level"""
 
-from .ap import *
-from .battlefield import *
-from .hit import *
-from .hp import *
-from .inspire import *
-from .kind import *
-from .xp import *
+from ..compat import *
+from ..entity import Entity, simplenode
+
+import math
+
+class LevelEntity(Entity):
+    @unbound
+    def _init(self, level=0):
+        self.dynamic_property('level', level)
+
+class XpEntity(Entity):
+    @unbound
+    def _init(self, xp=0):
+        self.req_mod(LevelEntity)
+        self.dynamic_property('xp', xp)
+        self.add_set_node('level', self.level_to_xp())
+        self.add_get_node('level', self.xp_to_level())
+    
+    @simplenode
+    def level_to_xp(self, value):
+        self.xp = ((2 ** value)-1)*100
+        return None
+    
+    @simplenode
+    def xp_to_level(self, value):
+        return math.log(self.xp / 100 + 1, 2)
