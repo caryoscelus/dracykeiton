7d989dd7aba73b07c763f262a214e0bc733bc480
caryoscelus <caryoscelus@gmx.com>
2016-04-12 19:38:42 +0300

    more doc for encounter package

--- dracykeiton/encounter/__init__.py
+++ dracykeiton/encounter/__init__.py
@@ -5,4 +5,7 @@ This package contains utils to build API for generating "menus". The menu
 can have it's own requirements and outcomes. It is possible to create separate
 menu object for each menu you generate or use single instance and clear it with
 .start method.
+
+option module contains several Option classes which you can combine & inherit to
+make your own custom Option.
 """
