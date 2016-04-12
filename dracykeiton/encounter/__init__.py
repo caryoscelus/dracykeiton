ca0bd7b7ce96b738ee28aa26389d16c00cc110d2
caryoscelus <caryoscelus@gmx.com>
2016-04-12 19:25:05 +0300

    encounter package docstring

---
+++ dracykeiton/encounter/__init__.py
@@ -0,0 +1,8 @@
+"""Utils to generate encounter menus
+
+This package contains utils to build API for generating "menus". The menu
+(represented by AdvancedMenu object) contains list of options and each of them
+can have it's own requirements and outcomes. It is possible to create separate
+menu object for each menu you generate or use single instance and clear it with
+.start method.
+"""
