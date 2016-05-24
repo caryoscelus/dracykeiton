c6aab32cdd56bc0a284ee4ab0147a4d83a74237f
caryoscelus <caryoscelus@gmx.com>
2016-05-24 15:15:48 +0300

    maybe

---
+++ dracykeiton/util/maybe.py
@@ -0,0 +1,33 @@
+##
+##  Copyright (C) 2016 caryoscelus
+##
+##  This file is part of Dracykeiton
+##  https://github.com/caryoscelus/dracykeiton
+##  
+##  This program is free software: you can redistribute it and/or modify
+##  it under the terms of the GNU General Public License as published by
+##  the Free Software Foundation, either version 3 of the License, or
+##  (at your option) any later version.
+##  
+##  This program is distributed in the hope that it will be useful,
+##  but WITHOUT ANY WARRANTY; without even the implied warranty of
+##  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+##  GNU General Public License for more details.
+##  
+##  You should have received a copy of the GNU General Public License
+##  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+##
+
+"""Helper functions to continue computation when result is not None."""
+
+from ..compat import *
+
+class maybe(object):
+    def __init__(self, value):
+        self.value = value
+    def __getattr__(self, attr):
+        if self.value is None:
+            def f(*args, **kwargs):
+                return None
+            return f
+        return getattr(self.value, attr)
